d40fba26f4b97bd29990d617918422407de6e390
David Hartman <david@petminded.com>
2015-07-01 22:01:13 -0700

    fixed add-to list

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ $(this).parents('p').remove();
 
 $('button.btn').click(function(noClick){
   noClick.preventDefault();
+  var addison = $('#todo').val();
      console.log('im awesome!');
-$(".list").append('<p><input type="checkbox"><i class="glyphicon glyphicon-star active"></i><span>' + Addison +'</span><i class="glyphicon glyphicon-remove"></i></p>');
+$(".list").append('<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + addison +'</span><i class="glyphicon glyphicon-remove"></i></p>');
  });
