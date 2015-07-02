667f10cec3cc008e7f4c3a9c0f8a58f72c6e8f80
David Hartman <david@petminded.com>
2015-07-01 22:14:35 -0700

    99% done

--- scripts.js
+++ scripts.js
@@ -1,14 +1,21 @@
-$('.glyphicon-star').on('click',function(){
-$(this).toggleClass('active');
-});
-
-$('.glyphicon-remove').on('click', function(){
-$(this).parents('p').remove();
-});
 
 $('button.btn').click(function(noClick){
-  noClick.preventDefault();
-  var addison = $('#todo').val();
-     console.log('im awesome!');
-$(".list").append('<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + addison +'</span><i class="glyphicon glyphicon-remove"></i></p>');
+      noClick.preventDefault();
+      var addison = $('#todo').val();
+      $(".list").append('<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + addison +'</span><i class="glyphicon glyphicon-remove"></i></p>');
+      $('.glyphicon-star').on('click',function(){
+        $(this).toggleClass('active');
+    });
+
+      $('.glyphicon-remove').on('click', function(){
+      $(this).parents('p').remove();
+    });
  });
+
+ var startSet = $('.glyphicon-star').on('click',function(){
+   $(this).toggleClass('active');
+   });
+
+ var itemRemove = $('.glyphicon-remove').on('click', function(){
+   $(this).parents('p').remove();
+   });
